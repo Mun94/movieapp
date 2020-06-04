2f1a66b6237e6f78f5690d59e342c4d281e5fab2
Mun Seong Woo <seongwoo720@gmail.com>
2020-06-04 21:51:50 +0900

    Rendering the Movies 및 movie data를 rating순으로 정렬

---
+++ src/Movie.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Movie({id, year, title, summary, poster}){
+    return(
+        <h5>{title}</h5>
+    )
+}
+
+Movie.prototype = {
+    id:PropTypes.number.isRequired,
+    title:PropTypes.string.isRequired,
+    year:PropTypes.number.isRequired,
+    summary:PropTypes.string.isRequired,
+    poster:PropTypes.string.isRequired
+}
+
+export default Movie;
