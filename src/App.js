8640ab1ed0a4bf3c5cbd4410e4d24512aee0dd32
Mun Seong Woo <seongwoo720@gmail.com>
2020-06-04 21:29:15 +0900

    Fetching(axios) movies data from API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div>
-      hello
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    isLoading : true,
+    movies:[]
+  }
+
+   getMovies = async() => {
+      const movie = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+   };
+
+   componentDidMount(){
+     this.getMovies();
+   }
+
+   render(){
+     const {isLoading} = this.state;
+      return <div>{isLoading ? "Loading" : "We are ready"}</div>
+   }
 }
 
 export default App;
